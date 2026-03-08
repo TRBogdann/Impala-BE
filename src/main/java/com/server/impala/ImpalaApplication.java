1f29008e491d75e3dbda1ee1ec5137da4f234823
TRBogdann <tr.bogdan50@gmail.com>
2026-03-08 15:25:40 +0200

    setup

---
+++ src/main/java/com/server/impala/ImpalaApplication.java
@@ -0,0 +1,13 @@
+package com.server.impala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpalaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/impala/ImpalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.impala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
